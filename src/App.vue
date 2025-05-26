519f4afd7c4a64e40332d5c25414b324a7d3dcc1
郭锋 <773835324@qq.com>
2025-05-26 03:07:48 +0000

    feat

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@
         <div v-for="item in col" :key="item.id" class="card">
           <video
             v-if="item.type === 'video'"
-            :poster="item.cover"
             controls
             muted
             playsinline
